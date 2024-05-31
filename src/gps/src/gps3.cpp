707861300c92f3d97cd0f516e2ce8996621fa5b1
Omar Ziad <O.Ziad@outlook.com>
2024-05-31 19:03:29 -0400

    Move bytesToReadNow to for loop in GPS_UBXI2C_Read (currently non-compiling code still)

--- src/gps/src/gps3.cpp
+++ src/gps/src/gps3.cpp
@@ -107,9 +107,9 @@ int GPS_UBXI2C_Read(const GPS_Device_t *obj, uint8_t *buffer, uint32_t capacity,
     // Initiate reads not exceeding max transfer length
     int bytesToRead = frameSize;
     uint8_t i2cReadBuffer[I2C_MAX_TRANSACTION];
-    for (int index = 0;;)
+    for (int bytesToReadNow, index = 0; bytesToReadNow > 0; )
     {
-        int bytesToReadNow = (bytesToRead > I2C_MAX_TRANSACTION) ? I2C_MAX_TRANSACTION : bytesToRead;
+        bytesToReadNow = (bytesToRead > I2C_MAX_TRANSACTION) ? I2C_MAX_TRANSACTION : bytesToRead;
         struct i2c_msg transactionSegment;
         GPS_UBXI2C_LoadRdSegment(&transactionSegment, i2cReadBuffer, bytesToReadNow);
         int status = GPS_UBXI2C_DoTransaction(obj, &transactionSegment, 1);
@@ -178,20 +178,26 @@ public:
     }
 };
 
+class UbxPayloadExcessException : public UbxDeserializerException
+{
+
+public:
+    // Only change is the constructor now contains the error message
+    explicit UbxPayloadExcessException(const std::vector<uint8_t> &payload, uint32_t currentIndex)
+        : UbxDeserializerException::UbxDeserializerException(payload, readIndex, payload, "Payload has more bytes.")
+    {
+    }
+};
+
+
 class UbxDeserializer
 {
 private:
     std::vector<uint8_t> payload;
     uint32_t index;
+    uint32_t bitfieldBuffer; 
 
-    void assertBytesAvailable(uint32_t length) {
-        uint32_t requiredLength = this->index + length; 
-        if(this->payload.size() < requiredLength) {
-            throw UbxPayloadDrainedException(this->payload, this->index, length); 
-        }
-    }
-
-    uint8_t readByte() {
+    uint8_t readByteNochk() {
         return this->payload.at(this->index++); 
     }
 
@@ -207,21 +213,36 @@ public:
         this->index = 0;
     }
 
+    void assertBytesAvailable(uint32_t length) {
+        uint32_t requiredLength = this->index + length; 
+        if(this->payload.size() < requiredLength) {
+            throw UbxPayloadDrainedException(this->payload, this->index, length); 
+        }
+    }
+
+    void assertEmpty()
+    {
+        if(this->index < this->payload.size())
+        {
+            throw UbxPayloadExcessException(this->payload); 
+        }
+    }
+
     uint8_t readU1() {
         this->assertBytesAvailable(1); 
-        return this->readByte(); 
+        return this->readByteNochk(); 
     }
 
     int8_t readS1() {
         this->assertBytesAvailable(1); 
-        return static_cast<int8_t>(this->readByte()); 
+        return static_cast<int8_t>(this->readByteNochk()); 
     }
 
     uint16_t readU2() {
         this->assertBytesAvailable(2); 
         uint16_t data; 
-        data  =  static_cast<uint16_t>(this->readByte()); 
-        data |= (static_cast<uint16_t>(this->readByte())) << 8; 
+        data  =  static_cast<uint16_t>(this->readByteNochk()); 
+        data |= (static_cast<uint16_t>(this->readByteNochk())) << 8; 
         return data; 
     }
 
@@ -232,10 +253,10 @@ public:
     uint32_t readU4() {
         this->assertBytesAvailable(4); 
         uint32_t data; 
-        data  =  static_cast<uint32_t>(this->readByte()); 
-        data |= (static_cast<uint32_t>(this->readByte())) << 8; 
-        data |= (static_cast<uint32_t>(this->readByte())) << 16; 
-        data |= (static_cast<uint32_t>(this->readByte())) << 24; 
+        data  =  static_cast<uint32_t>(this->readByteNochk()); 
+        data |= (static_cast<uint32_t>(this->readByteNochk())) << 8; 
+        data |= (static_cast<uint32_t>(this->readByteNochk())) << 16; 
+        data |= (static_cast<uint32_t>(this->readByteNochk())) << 24; 
         return data; 
     }
 
@@ -246,8 +267,17 @@ public:
     void readByteArray(uint8_t * data, uint32_t length) {
         this->assertBytesAvailable(length); 
         for(int i = 0; i < length; i++) {
-            data[i] = this->readByte(); 
+            data[i] = this->readByteNochk(); 
         }
+    }
+
+    std::vector<uint8_t> readVector(uint32_t length)
+    {
+        this->assertBytesAvailable(length); 
+        std::vector<uint8_t> data; 
+        data.resize(length); 
+        this->readByteArray(data.data(), data.size()); 
+        return data; 
     }
 
     float readR4() {
@@ -264,7 +294,70 @@ public:
     
     char readCh() {
         this->assertBytesAvailable(1); 
-        return static_cast<char>(this->readByte()); 
+        return static_cast<char>(this->readByteNochk()); 
     }
 
-}; 
+    void readX1() {
+        this->bitfieldBuffer = static_cast<uint32_t>(this->readU1()); 
+    }
+
+    void readX2() {
+        this->bitfieldBuffer = static_cast<uint32_t>(this->readU2()); 
+    }
+
+    void readX4() {
+        this->bitfieldBuffer = this->readU4(); 
+    }
+
+    bool readXF(int bit) {
+        if(bit < 0 || bit > 31)
+        {
+            return false; 
+        }
+        return !!(this->bitfieldBuffer & (1 << bit)); 
+    }
+
+    uint32_t readXU(int msb, int lsb) {
+        if (msb < 0 || lsb < 0 || msb > 31 || lsb > 31 || msb < lsb)
+        {
+            return 0;
+        }
+        uint32_t temp = this->bitfieldBuffer >> lsb; 
+        if(msb < 31)
+        {
+            uint32_t mask = (1 << msb - lsb + 1) - 1; 
+            return temp & mask; 
+        }
+        else
+        {
+            return temp; 
+        }
+    }
+
+    int32_t readXI(int msb, int lsb) {
+        if (msb < 0 || lsb < 0 || msb > 31 || lsb > 31 || msb < lsb)
+        {
+            return 0;
+        }
+        int32_t temp = static_cast<int32_t>(this->bitfieldBuffer) >> lsb; 
+        if(msb < 31)
+        {
+            uint32_t mask = (1 << msb - lsb + 1) - 1; 
+            return temp & mask; 
+        }
+        else
+        {
+            return temp; 
+        }
+    }
+
+    int32_t readXS(int msb, int lsb) {
+        if (msb < 0 || lsb < 0 || msb > 31 || lsb > 31 || msb <= lsb) // Note the <=, we need at least 2 bits
+        {
+            return 0;
+        }
+        uint32_t magnitude = this->readXU(msb - 1, lsb); 
+        return this->readXF(msb) ? -magnitude : magnitude; 
+    }
+
+};
