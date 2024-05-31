f8d83cb74cc73b59ae9b05c81d4aec17238751f0
OZiad <O.Ziad@outlook.com>
2024-05-31 14:15:03 -0400

    minor tweaks

--- launch/robot_ik.py
+++ launch/robot_ik.py
@@ -38,7 +38,7 @@ def generate_launch_description():
                 {'joint_lengths': [1.354, 1.333, 1.250]},
                 {'joint_angle_mins': [-180.0, -80.0, -111.0, -101.0]},
                 {'joint_angle_maxes': [180, 80.0, 115.0, 106.0]},
-                {'sensitivity': 1.0},
+                {'sensitivity': 0.75},
                 {'mode': '2D'},
                 {'solution': 1},
                 # "joint" sets final joint angle, while "vertical" sets the
