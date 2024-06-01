dc26e7865fd50ef4b9110c632bb10f9b71de6984
Omar Ziad <O.Ziad@outlook.com>
2024-06-01 15:15:17 -0400

    Change tests to be more readable by reading payload from end to start and checking for hex values

--- src/gps/Google_tests/serializerTest.cpp
+++ src/gps/Google_tests/serializerTest.cpp
@@ -1,19 +1,25 @@
 #include "gtest/gtest.h"
 #include "serdes/UbxSerializer.hpp"
 
-TEST(UbxSerializerTest, givenSerializerObjectIsInstantiated_whenDefaultConstructorIsUsed_thenPayloadIsEmpty)
+// Since we are writting in little endian, we will read from the end of the payload to the start
+class UbxSerializerTest : public testing::Test
 {
-    UbxSerializer serializer;
-    EXPECT_EQ(serializer.getPayloadReadOnly().size(), 0);
+protected:
+    UbxSerializer serializer_;
+};
+
+TEST_F(UbxSerializerTest, givenSerializerObjectIsInstantiated_whenDefaultConstructorIsUsed_thenPayloadIsEmpty)
+{
+    EXPECT_EQ(serializer_.getPayloadReadOnly().size(), 0);
 }
 
-TEST(UbxSerializerTest, givenSerializerObjectIsInstantiated_whenSizeIsGiven_thenPayloadIsCorrectSize)
+TEST_F(UbxSerializerTest, givenSerializerObjectIsInstantiated_whenSizeIsGiven_thenPayloadIsCorrectSize)
 {
     UbxSerializer serializer(10);
     EXPECT_EQ(serializer.getPayloadReadOnly().capacity(), 10);
 }
 
-TEST(UbxSerializerTest, givenEmptyPayload_whenWriteU1_thenPayloadContainsIntBetween0And255)
+TEST_F(UbxSerializerTest, givenEmptyPayload_whenWriteU1_thenPayloadContainsHexRepresentation)
 {
     // Arrange
     UbxSerializer serializer;
@@ -21,61 +27,67 @@ TEST(UbxSerializerTest, givenEmptyPayload_whenWriteU1_thenPayloadContainsIntBetw
     const int indicesUsedPerWrite = 1;
 
     //Act
-    serializer.writeU1(0x12); // 0x12 = 18
+    serializer.writeU1(18);
     serializer.writeU1(1);
     serializer.writeU1(-1);
 
     // Assert
     ASSERT_EQ(payload.size(), indicesUsedPerWrite * 3);
-    EXPECT_EQ(payload[0], 18);
-    EXPECT_EQ(payload[1], 1);
-    EXPECT_EQ(payload[2], 255);
+    EXPECT_EQ(payload[2], 0xFF);
+    EXPECT_EQ(payload[1], 0x01);
+    EXPECT_EQ(payload[0], 0x12);
 }
 
-TEST(UbxSerializerTest, givenEmptyPayload_whenWriteU2_thenPayloadContainsIntBetween0And65535)
+TEST_F(UbxSerializerTest, givenEmptyPayload_whenWriteU2_thenPayloadContainsHexRepresentation)
 {
     // Arrange
     UbxSerializer serializer;
     const auto& payload = serializer.getPayloadReadOnly();
-    const int indicesUsedPerWrite = 2;
+    constexpr int indicesUsedPerWrite = 2;
 
     //Act
-    serializer.writeU2(0x11); // 0x11 = 17
+    serializer.writeU2(17); // 0x11 = 17
     serializer.writeU2(1);
     serializer.writeU2(-1); // -1 -> 65535
 
     // Assert
     ASSERT_EQ(payload.size(), indicesUsedPerWrite * 3);
-    EXPECT_EQ(payload[0], 17);
+    EXPECT_EQ(payload[5], 0xFF);
+    EXPECT_EQ(payload[4], 0xFF);
+    EXPECT_EQ(payload[3], 0x00);
+    EXPECT_EQ(payload[2], 0x01);
     EXPECT_EQ(payload[1], 0);
-    EXPECT_EQ(payload[2], 1);
-    EXPECT_EQ(payload[3], 0);
-    EXPECT_EQ(payload[4], 255);
-    EXPECT_EQ(payload[5], 255);
+    EXPECT_EQ(payload[0], 0x11);
 }
 
-TEST(UbxSerializerTest, givenEmptyPayload_whenWriteU4_thenPayloadContainsIntBetween0And65535)
+TEST_F(UbxSerializerTest, givenEmptyPayload_whenWriteU4_thenPayloadContainsHexRepresentation)
 {
     // Arrange
     UbxSerializer serializer;
     const auto& payload = serializer.getPayloadReadOnly();
-    const int indicesUsedPerWrite = 4;
+    constexpr int indicesUsedPerWrite = 4;
 
     //Act
-    serializer.writeU4(0x24); // 0x24 = 36
-    // serializer.writeU4(-1); // -1 -> 2,147,483,647
-    serializer.writeU4(0x7FFFFFFF); // = 2,147,483,647
-
+    serializer.writeU4(36); // 0x24 = 36
+    serializer.writeU4(-1); // 0xFFFFFFFF = -1
+    serializer.writeU4(2147483647); // 0x7FFFFFF = 2147483647
 
     // Assert
-    ASSERT_EQ(payload.size(), indicesUsedPerWrite * 2);
-    EXPECT_EQ(payload[0], 36);
-    EXPECT_EQ(payload[1], 0);
-    EXPECT_EQ(payload[2], 0);
-    EXPECT_EQ(payload[3], 0);
+    ASSERT_EQ(payload.size(), indicesUsedPerWrite * 3);
 
-    EXPECT_EQ(payload[4], 255);
-    EXPECT_EQ(payload[5], 255);
-    EXPECT_EQ(payload[6], 255);
-    EXPECT_EQ(payload[7], 255);
+
+    EXPECT_EQ(payload[11], 0x7F);
+    EXPECT_EQ(payload[10], 0xFF);
+    EXPECT_EQ(payload[9], 0xFF);
+    EXPECT_EQ(payload[8], 0xFF);
+
+    EXPECT_EQ(payload[7], 0xFF);
+    EXPECT_EQ(payload[6], 0xFF);
+    EXPECT_EQ(payload[5], 0xFF);
+    EXPECT_EQ(payload[4], 0xFF);
+
+    EXPECT_EQ(payload[3], 0);
+    EXPECT_EQ(payload[2], 0);
+    EXPECT_EQ(payload[1], 0);
+    EXPECT_EQ(payload[0], 0x24);
 }
