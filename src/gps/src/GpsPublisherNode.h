4ca19ba9eb50a1347424199108f4a377205a3a3d
Omar Ziad <O.Ziad@outlook.com>
2024-06-07 10:43:27 -0400

    Change naming to align with ros2 and c++ convention

--- src/gps/src/GpsPublisherNode.h
+++ src/gps/src/GpsPublisherNode.h
@@ -9,25 +9,24 @@
 
 using sensor_msgs::msg::NavSatFix;
 
-struct gpsData
+struct GpsData
 {
 	float latitude;
 	float longitude;
 	float altitude;
 };
 
-class gpsPublisherNode : public rclcpp::Node
+class GpsPublisherNode : public rclcpp::Node
 {
- public:
-	explicit gpsPublisherNode(const std::string& gpsTopic = "/gps_data");
+public:
+	explicit GpsPublisherNode(const std::string& gpsTopic = "/gps_data");
 
- private:
+private:
 	void publishGpsData();
-	gpsData extractGpsData();
+	GpsData extractGpsData();
 	const float MULTIPLYING_FACTOR = 1e-7;
 
 	rclcpp::TimerBase::SharedPtr timer_;
 	rclcpp::Publisher<NavSatFix>::SharedPtr publisher_;
 	SAM_M8Q_GPS gps_;
-
 };
