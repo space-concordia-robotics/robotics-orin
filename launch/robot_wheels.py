015b2dd096274c1899b653cf853e2ca8a2bb4352
Marc Scattolin <marcscattolin@gmail.com>
2024-06-21 13:14:35 -0400

    fix robot_wheels.py

--- launch/robot_wheels.py
+++ launch/robot_wheels.py
@@ -27,41 +27,9 @@ def generate_launch_description():
         namespace='/',
     )
 
-    configure_event = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node=wheels_controller_driver, goal_state='configured',
-            entities=[
-                LogInfo(
-                    msg="[LifecycleLaunch] wheels controller node is configuring."),
-                EmitEvent(event=ChangeState(
-                    lifecycle_node_matcher=matches_action(wheels_controller_driver),
-                    transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
-                )),
-            ],
-        )
-    )
-
-
-    urdf_file_name = 'astro_arm.urdf'
-    urdf = os.path.join(
-        get_package_share_directory('arm_ik'),
-        urdf_file_name)
-    with open(urdf, 'r') as infp:
-        robot_desc = infp.read()
 
     return LaunchDescription([
-        # Node(
-        #     package='arm_ik',
-        #     executable='CadMouseJoyNode',
-        #     name='cad_mouse_joy_node',
-        #     output='screen',
-        #     parameters=[
-        #         # More deadzone on yaw (pivot)
-        #         {'deadzones': [20, 20, 20, 20, 20, 200]}
-        #     ]
-        # ),
         wheels_controller_driver,
-        configure_event,
         LifecycleNode(
             package='joy',
             executable='joy_node',
